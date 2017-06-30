ed2e14af0b889d4ede9d55a03dc1dcf9d1174798
David D'Agostino <davedags@gmail.com>
2017-06-30 00:05:09 -0400

    remove console log

--- src/app/coins/coins.service.ts
+++ src/app/coins/coins.service.ts
@@ -60,7 +60,6 @@ export class CoinsService {
                         totalMarketCap: marketCap,
                         coins: collection
                     };
-                    console.log(returnVal);
                     return returnVal;
                 })
             .catch(
