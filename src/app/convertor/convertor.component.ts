26ef316d94f2a0c55c86dee75b0c00aa9c18911e
David D'Agostino <davedags@gmail.com>
2017-07-28 00:24:04 -0400

    add convertor

--- src/app/convertor/convertor.component.ts
+++ src/app/convertor/convertor.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CoinDetailService } from '../coin-detail/coin-detail.service';
 
 @Component({
-  selector: 'app-convertor',
-  templateUrl: './convertor.component.html',
-  styleUrls: ['./convertor.component.css']
+    selector: 'app-convertor',
+    templateUrl: './convertor.component.html',
+    styleUrls: ['./convertor.component.css'],
+    providers: [
+        CoinDetailService
+    ]
 })
 export class ConvertorComponent implements OnInit {
+    coinAmount: number = 1;
+    coinSymbol: string = 'BTC';
+    coinBasePrice: any = 'loading ...';
+    fiatAmount: any = 'loading ...'
+    loaded: boolean = false;
 
-  constructor() { }
+    constructor(private coinService: CoinDetailService) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
 
+        this.coinService.getPrice(this.coinSymbol)
+        .subscribe(price => {
+            this.coinBasePrice = price;
+            this.fiatAmount = this.formatNumber(this.coinBasePrice);
+        });
+    }
+
+    updateFiatPrice() {
+        if (this.coinBasePrice) {
+            if (this.coinAmount <= 0) {
+                this.fiatAmount = 0;
+            } else if (this.coinAmount > 0) {
+                let amount = this.coinAmount * this.coinBasePrice;
+                this.fiatAmount = this.formatNumber(amount);
+            }
+        }
+    }
+
+    formatNumber(num) {
+        return Number(num).toLocaleString('en-US', { maximumFractionDigits: 0 });
+    }
 }
